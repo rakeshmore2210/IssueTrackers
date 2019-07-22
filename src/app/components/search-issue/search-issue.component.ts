6922ef5f0f108070526fa993a5379df51b7d4272
Rakesh More <rakesh.more@zensar.com>
2019-07-23 00:15:21 +0530

    Main Components

---
+++ src/app/components/search-issue/search-issue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-issue',
+  templateUrl: './search-issue.component.html',
+  styleUrls: ['./search-issue.component.css']
+})
+export class SearchIssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
